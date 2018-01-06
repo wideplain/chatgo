200bd78545e01669eeb843b6f9dda55ec62e6d5f
KoyaKikuchi <koyakikuchi@wideplain.com>
2018-01-06 10:41:45 +0900

    first commit

---
+++ src/main/java/com/chatgo/ChatgoApplication.java
@@ -0,0 +1,12 @@
+package com.chatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgoApplication.class, args);
+	}
+}

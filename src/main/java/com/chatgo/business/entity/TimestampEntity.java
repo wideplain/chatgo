5b7b362be07e67a83847c465b8bee1b52d52651c
KoyaKikuchi <koyakikuchi@wideplain.com>
2018-01-17 00:12:04 +0900

    TimestampEntityの追加
    UserのTimestampの追加

---
+++ src/main/java/com/chatgo/business/entity/TimestampEntity.java
@@ -0,0 +1,30 @@
+package com.chatgo.business.entity;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.util.Date;
+import java.sql.Timestamp;
+
+@MappedSuperclass
+public abstract class TimestampEntity {
+
+    public Timestamp updatedAt;
+
+    @Column(updatable=false)
+    public Timestamp createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        Timestamp ts = new Timestamp((new Date()).getTime());
+        this.createdAt = ts;
+        this.updatedAt = ts;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = new Timestamp((new Date()).getTime());
+    }
+
+}
---
+++ src/main/java/com/chatgo/business/entity/User.java
@@ -0,0 +1,6 @@
+package com.chatgo.business.entity;
+
+public class User extends TimestampEntity {
+
+    
+}

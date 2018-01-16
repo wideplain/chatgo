5f7de1b75c5d39b5884bb5ab57cb2c1e5d051b83
KoyaKikuchi <koyakikuchi@wideplain.com>
2018-01-17 00:26:17 +0900

    add user entity
    ユーザーテーブルの追加

--- src/main/java/com/chatgo/business/entity/User.java
+++ src/main/java/com/chatgo/business/entity/User.java
@@ -1,6 +1,63 @@
 package com.chatgo.business.entity;
 
-public class User extends TimestampEntity {
+import javax.persistence.*;
 
-    
+@Entity
+@Table(name = "users")
+public class User extends TimestampEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private String profileImage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
 }
+

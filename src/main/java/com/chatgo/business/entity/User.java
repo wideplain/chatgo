3caefb25c51ca8eaf8f6038c325673e745578bb8
KoyaKikuchi <koyakikuchi@wideplain.com>
2018-01-18 21:49:56 +0900

    timestamp
    timestamp classのextends

--- src/main/java/com/chatgo/business/entity/User.java
+++ src/main/java/com/chatgo/business/entity/User.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends TimestampEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
